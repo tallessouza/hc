4561bc0f4a565abd6f5945581948f7bad2c3b10c
tallessouza <talles-swd@live.com>
2022-04-27 23:26:35 -0300

    Projeto

---
+++ script.js
@@ -0,0 +1,6 @@
+function displayAbout() {
+  var icon = document.getElementById('animation')
+  icon.classList.toggle('rotate')
+  var display = document.getElementById('about')
+  display.classList.toggle('active')
+}
